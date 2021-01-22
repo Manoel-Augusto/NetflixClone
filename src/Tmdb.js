03908f9dc0a2bb30aff1b3576e24ad626829aaba
Manoel-Augusto <manoelll@gmail.com>
2021-01-22 15:45:28 -0300

    Commit inicial preparação do projeto e arquivo de comunicação com a api tmdb

---
+++ src/Tmdb.js
@@ -0,0 +1,16 @@
+const API_KEY='cf7e4685aecedd9c40538f30cb7a197a';
+const API_BASE='https://api.themoviedb.org/3';
+
+/*
+-originais da netflix
+-recomendados(trending)
+-em alta(top rated)
+-ação
+-comedia
+-terror
+-romance
+-documentários
+*/
+export default{
+
+}
