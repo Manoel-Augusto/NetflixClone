8e1fc5c0edcc2b94e927874742b1839c7e8e1f64
Manoel-Augusto <manoelll@gmail.com>
2021-01-25 17:05:41 -0300

    implementação da tela com as listagens de cada genero

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -11,6 +11,55 @@ const API_BASE='https://api.themoviedb.org/3';
 -romance
 -documentários
 */
-export default{
 
+const basicFetch=async(endpoint)=>{
+    const req=await fetch(`${API_BASE}${endpoint}`);
+    const Json=await req.json();
+    return Json;
+}
+export default{
+getHomeList: async()=>{
+    return[
+        {
+            slug:'originals',
+            title:'Originais do Netfilx',
+            items:await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+        },
+        {
+            slug:'trending',
+            title:'Recomendados para Você',
+            items:await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+        },
+        {
+            slug:'toprated',
+            title:'Em Alta',
+            items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+        },
+        {
+            slug:'action',
+            title:'Ação',
+            items:await basicFetch(`/discover/movie?witch_genres=28&language=pt-BR&api_key=${API_KEY}`)
+        },
+        {
+            slug:'comedy',
+            title:'Comédia',
+            items:await basicFetch(`/discover/movie?witch_genres=35&language=pt-BR&api_key=${API_KEY}`)
+        },
+        {
+            slug:'horror',
+            title:'Terror',
+            items:await basicFetch(`/discover/movie?witch_genres=27&language=pt-BR&api_key=${API_KEY}`)
+        },
+        {
+            slug:'romance',
+            title:'Romance',
+            items:await basicFetch(`/discover/movie?witch_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+        },
+        {
+            slug:'Documentary',
+            title:'Documentários',
+            items:await basicFetch(`/discover/movie?witch_genres=99&language=pt-BR&api_key=${API_KEY}`)
+        },
+    ];
+}
 }
