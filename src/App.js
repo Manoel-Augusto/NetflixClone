a7bc703b6d9c03f89d6c0c4af71b729c76433ff3
Manoel-Augusto <manoelll@gmail.com>
2021-01-29 23:10:34 -0300

    implementação do footer, responsividade, descrição resumida

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import Header from './components/Header';
 export default () => {
   const [movieList, setMovieList] = useState([]);
 
-  const[featuredData, setFeaturedData]=useState(null);
+  const [featuredData, setFeaturedData] = useState(null);
 
-  const [balckHeader, setBlackHeader]=useState(false);
-  
+  const [balckHeader, setBlackHeader] = useState(false);
+
 
   useEffect(() => {
     const loadAll = async () => {
@@ -20,41 +20,52 @@ export default () => {
       setMovieList(list);
 
       // Pegando o Featured
-      let originals = list.filter(i=>i.slug==='originals');
-      let randomChosen=Math.floor(Math.random()*(originals[0].items.results.length-1));
-      let chosen=originals[0].items.results[randomChosen];
-      let chosenInfo= await Tmdb.getMovieInfo(chosen.id, 'tv');
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
       setFeaturedData(chosenInfo);
     }
     loadAll();
   }, []);
 
-  useEffect(()=>{
-    const scrollListener = ()=>{
-if(window.scrollY>30){
-  setBlackHeader(true);
-} else {
-  setBlackHeader(false);
-}
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 30) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
     }
     window.addEventListener('scroll', scrollListener);
-    return()=>{
-window.removeEventListener('scroll', scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
     }
-  },[])
+  }, [])
 
   return (
     <div className='page'>
-      <Header black={balckHeader}/>
-      { featuredData && 
-      <FeaturedMovie item={featuredData}/>
+      <Header black={balckHeader} />
+      { featuredData &&
+        <FeaturedMovie item={featuredData} />
       }
       <section className='lists'>
-        {movieList.map((item, key)=>(
-          <MovieRow key={key} title={item.title} items={item.items}/>           
-          
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+
         ))}
       </section>
+      <footer>
+      <span role="img" aria-label="sorriso">&#128578;</span> Clone da página principal do Netflix<span role="img" aria-label="sorriso">&#128578;</span>  <br /><br/>
+        Direitos de imagem para Netflix <br />
+        Dados pegos do site Themoviedb.org
+      </footer>
+
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif" alt="Carregando" />
+        </div>
+      }
 
     </div>
   );
